c81c4c25d098fafa8c79c9983b1faa37318aee18
BucleEterno333 <Bucleterno333@gmail.com>
2026-06-16 01:28:53 -0600

    a

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const { pool, initDatabase } = require('./database');
-const loggingMiddleware = require('./middleware/loggingMiddleware');
 
 const app = express();
 
