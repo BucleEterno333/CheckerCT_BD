968f7eca2b4c1153f72e0d89c0a9fb2ccee4df81
BucleEterno333 <Bucleterno333@gmail.com>
2026-06-21 10:51:02 -0600

    a

--- routes/telegram.js
+++ routes/telegram.js
@@ -73,7 +73,7 @@ router.post('/send-live', async (req, res) => {
 // ============================================
 // ENDPOINT 2: Guardar live en base de datos
 // ============================================
-const { upsertLive } = require('../models/UserLive');
+const { upsertLive } = require('../models/Live');
 
 router.post('/save-live', async (req, res) => {
     try {
